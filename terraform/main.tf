45bd1b327b39f076549d623282547df2d41501fd
prasad-sawant1 <prasad.sawant1@ingka.com>
2023-01-16 12:33:58 +0100

    added new bucket

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,3 +2,9 @@ resource "google_storage_bucket" "bucket" {
   name     = "test-bucket-random-110784"
   location = "europe-west2"
 }
+
+
+resource "google_storage_bucket" "bucket" {
+  name     = "test-bucket-random-anay"
+  location = "europe-west2"
+}
