7155492692ccb4ba3c22876ef08bd06fd0294a1c
prasad-sawant1 <prasad.sawant1@ingka.com>
2023-02-25 20:04:11 +0100

    streaming dataflow

--- dataflow/streaming-dataflow/fixed-window/terraform/main.tf
+++ dataflow/streaming-dataflow/fixed-window/terraform/main.tf
@@ -3,7 +3,31 @@ resource "google_storage_bucket" "bucket" {
   location = "europe-west2"
 }
 
-resource "google_pubsub_topic" "orders" {
+resource "google_pubsub_topic" "topic" {
   name = "orders"
   message_retention_duration = "86600s"
+}
+
+resource "google_pubsub_subscription" "subscription" {
+  name  = "orders-sub"
+  topic = google_pubsub_topic.topic.orders
+
+  labels = {
+    purpose = "streaming-dataflow"
+  }
+
+  # 20 minutes
+  message_retention_duration = "1200s"
+  retain_acked_messages      = true
+
+  ack_deadline_seconds = 20
+
+  expiration_policy {
+    ttl = "300000.5s"
+  }
+  retry_policy {
+    minimum_backoff = "10s"
+  }
+
+  enable_message_ordering    = false
 }
