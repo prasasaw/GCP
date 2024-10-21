f2eb0e66a3a239b7476f4240a4b5477f5aa330bb
Prasad Sawant <prasad.sawant1@ingka.com>
2024-10-21 10:21:12 +0530

    slight change

--- cloud-run/my-cloud-run/main.py
+++ cloud-run/my-cloud-run/main.py
@@ -1,3 +1,15 @@
+"""
+This module contains a simple FastAPI application with a single endpoint.
+The application responds to GET requests at the root URL ("/") with a JSON message.
+The message can be customized using the "name" query parameter.
+Functions:
+- hello_there: Returns a greeting string.
+- hello_world: Handles GET requests to the root URL and returns a JSON response.
+- start: Starts the FastAPI application using Uvicorn.
+Usage:
+Run this module directly to start the FastAPI application.
+"""
+
 import os
 import uvicorn
 from fastapi import FastAPI, Request
