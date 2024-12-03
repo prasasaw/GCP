1eb2513a8272a3eb07c93218b5df4c5608c68cc8
Prasad Sawant <prasad.sawant1@ingka.com>
2024-12-03 10:49:18 +0530

    added artifact reop

--- terraform/main.tf
+++ terraform/main.tf
@@ -4,8 +4,8 @@ resource "google_storage_bucket" "tf-state-bucket" {
 }
 
 resource "google_artifact_registry_repository" "artifact-repo" {
-  provider = google-beta
   location = "europe-west1"
   repository_id = "repo-prasad-gcp4"
-  format = "DOCKER"
+  description   = "docker repository"
+  format        = "DOCKER"
 }
